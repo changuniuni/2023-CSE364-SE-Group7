a49b5bfcff53c3a4062ecadae81c8811613f23c7
kunamin <47911730+kunamin@users.noreply.github.com>
2023-03-30 22:03:53 +0900

    Part II: Setup Spring Boot and Create REST APIs

---
+++ src/test/java/cse364project1/SNAPSHOTjarwithdependencies/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cse364project1.SNAPSHOTjarwithdependencies.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
