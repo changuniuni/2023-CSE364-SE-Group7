6db7ab401c5e5176a22e125fa285bcdf88297cdf
KyeongHoon, Min <gi0557@naver.com>
2023-04-05 18:56:16 +0000

    Refactoring & Fix pom.xml

--- src/main/java/milestone1/combined/EmployeeNotFoundException.java
+++ src/main/java/milestone1/combined/EmployeeNotFoundException.java
@@ -1,7 +1,7 @@
 package milestone1.combined;
 
 class EmployeeNotFoundException extends RuntimeException {
-
+ 
   EmployeeNotFoundException(String id) {
     super("Could not find employee " + id);
   }
--- src/main/java/milestone1/combined/LoadDatabase.java
+++ src/main/java/milestone1/combined/LoadDatabase.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+ 
 @Configuration
 class LoadDatabase {
 
