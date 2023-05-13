e8121136a0c7369d076143d2027e7b4d19c574c8
changuniuni <48553632+changuniuni@users.noreply.github.com>
2023-05-13 14:24:57 +0000

    jacoco report success

--- src/test/java/milestone2/UserTest.java
+++ src/test/java/milestone2/UserTest.java
@@ -1,38 +1,55 @@
-// package milestone2;
+package milestone2;
 
-// import milestone2.courses.Course;
-// import milestone2.sign_up.model.User;
+import milestone2.courses.Course;
+import milestone2.sign_up.model.User;
 
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-// public class UserTest {
-// // Give the junit test code for the  User.java file.
-//     private User user;
-//     private Course course;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-//     @BeforeEach
-//     public void setUp() {
-//         user = new User("1", "John");
-//         course = new Course(null, null, null, null, null, null, 0, null, null);
-//     }
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-//     @Test
-//     public void testGetId() {
-//         assertEquals("1", user.getId());
-//     }
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-//     @Test
-//     public void testGetName() {
-//         assertEquals("John", user.getName());
-//     }
 
-//     @Test
-//     public void testSetName() {
-//         user.setName("Jane");
-//         assertEquals("Jane", user.getName());
-//     }
-  
-// }
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserTest {
+// Give the junit test code for the  User.java file.
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User("1", "John");
+    }
+
+    @Test
+    public void testGetId() {
+        try{
+            assertEquals("1", user.getId());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetName() {
+        try{
+            assertEquals("John", user.getName());
+        } catch (Exception e) {
+            fail();
+        }
+    }  
+}
