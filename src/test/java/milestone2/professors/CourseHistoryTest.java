659e170b733cb935e997a572d19642a305981d80
Kunamin <mkh533@unist.ac.kr>
2023-05-14 08:02:31 +0000

    Professor.java test add two test

---
+++ src/test/java/milestone2/professors/CourseHistoryTest.java
@@ -0,0 +1,65 @@
+package milestone2.professors;
+
+import org.junit.jupiter.api.Test;
+
+public class CourseHistoryTest {
+    @Test
+    void testGetCourseId() {
+
+    }
+
+    @Test
+    void testGetId() {
+
+    }
+
+    @Test
+    void testGetOpenSmes() {
+
+    }
+
+    @Test
+    void testGetOpenYear() {
+
+    }
+
+    @Test
+    void testGetProfessorId() {
+
+    }
+
+    @Test
+    void testGetProfessorName() {
+
+    }
+
+    @Test
+    void testSetCourseId() {
+
+    }
+
+    @Test
+    void testSetId() {
+
+    }
+
+    @Test
+    void testSetOpenSmes() {
+
+    }
+
+    @Test
+    void testSetOpenYear() {
+
+    }
+
+    @Test
+    void testSetProfessorId() {
+
+    }
+
+    @Test
+    void testSetProfessorName() {
+
+    }
+}
