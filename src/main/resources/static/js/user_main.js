3d172dd8a95ecd1c9c42f500708e861b0c327274
changuniuni <48553632+changuniuni@users.noreply.github.com>
2023-06-17 07:37:11 +0000

    professor fin

--- src/main/resources/static/js/user_main.js
+++ src/main/resources/static/js/user_main.js
@@ -1,62 +1,56 @@
 $(document).ready(function() {
-    // Handle professor link click
-    $('.nav-item-professor').click(function(e) {
-      e.preventDefault();
+    // Fetch the professors' information
+    $.ajax({
+      url: 'http://localhost:8080/professors',
+      type: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        // Extract the relevant information from the response
+        const professors = response._embedded.professors;
+        const professorData = professors.map(function(professor) {
+          const { name, topic, email } = professor;
+          return { name, topic, email };
+        });
   
-      // Send a GET request to fetch the professor information from the server
-      $.ajax({
-        url: 'http://localhost:8080/professors',
-        type: 'GET',
-        dataType: 'json',
-        success: function(response) {
-          console.log('Professors retrieved:', response);
-          // Display the professor information in the user main content section
-          showProfessorInformation(response);
-        },
-        error: function(error) {
-          console.log('Error retrieving professors:', error);
-          // Handle error response here
-        }
-      });
+        // Display the professor information in a table
+        displayProfessorTable(professorData);
+      },
+      error: function(error) {
+        console.log('Error:', error);
+      }
     });
   
-    // Function to display the professor information
-    function showProfessorInformation(professors) {
-        // Clear the existing content in the user main content section
-        $('.user-main-content').empty();
-      
-        // Create the table element
-        const table = $('<table>').addClass('professor-table');
-        const thead = $('<thead>');
-        const tbody = $('<tbody>');
-      
-        // Create the table headers
-        const headers = ['Name', 'Area', 'Topic', 'Email', 'Office'];
-        const headerRow = $('<tr>');
-        headers.forEach(function(header) {
-          const th = $('<th>').text(header);
-          headerRow.append(th);
-        });
-        thead.append(headerRow);
-        table.append(thead);
-      
-        // Create the table rows with professor data
-        professors.forEach(function(professor) {
-          const { name, area, topic, email, office } = professor;
-          const row = $('<tr>');
-          const nameCell = $('<td>').text(name);
-          const areaCell = $('<td>').text(area.join(', ')); // Join the area array with a comma separator
-          const topicCell = $('<td>').text(topic);
-          const emailCell = $('<td>').text(email);
-          const officeCell = $('<td>').text(office);
-          row.append(nameCell, areaCell, topicCell, emailCell, officeCell);
-          tbody.append(row);
-        });
-      
-        // Append the table to the user main content section
-        table.append(tbody);
-        $('.user-main-content').append(table);
-      }
-      
+    // Function to display the professor information in a table
+    function displayProfessorTable(professors) {
+      // Create the table element
+      const table = $('<table>').addClass('professor-table');
+      const thead = $('<thead>');
+      const tbody = $('<tbody>');
+  
+      // Create the table headers
+      const headers = ['Name', 'Topic', 'Email'];
+      const headerRow = $('<tr>');
+      headers.forEach(function(header) {
+        const th = $('<th>').text(header);
+        headerRow.append(th);
+      });
+      thead.append(headerRow);
+      table.append(thead);
+  
+      // Create the table rows with professor data
+      professors.forEach(function(professor) {
+        const { name, topic, email } = professor;
+        const row = $('<tr>');
+        const nameCell = $('<td>').text(name);
+        const topicCell = $('<td>').text(topic);
+        const emailCell = $('<td>').text(email);
+        row.append(nameCell, topicCell, emailCell);
+        tbody.append(row);
+      });
+  
+      // Append the table to the screen
+      table.append(tbody);
+      $('.user-main-content').append(table);
+    }
   });
   
