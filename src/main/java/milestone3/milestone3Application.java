bf145b001a3d6b6820bb2f06c88f34d9c7b111f1
root <mkh533@unist.ac.kr>
2023-06-18 11:37:04 +0000

    add SpringBootServletInitializer

--- src/main/java/milestone3/milestone3Application.java
+++ src/main/java/milestone3/milestone3Application.java
@@ -2,9 +2,10 @@ package milestone3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class milestone3Application {
+public class milestone3Application extends SpringBootServletInitializer  {
 
   public static void main(String... args) {
     SpringApplication.run(milestone3Application.class, args);
